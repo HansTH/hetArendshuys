af3307ad5daa5d44d774ba036e53b0a11afd987a
Hans ter Horst <hbhorst@mac.com>
2020-12-21 13:21:43 +0100

    create 404 page

--- gatsby/src/pages/404.js
+++ gatsby/src/pages/404.js
@@ -1,5 +1,42 @@
 import React from 'react';
+import styled from 'styled-components';
+import notFoundImage from '../assets/images/404image.jpg';
+import SEO from '../components/SEO';
+import { breakpoint } from '../styles/breakpoints';
 
-export default function Page404() {
-	return <div>Page Not Found!</div>;
-}
+const PageNotFound = () => (
+	<>
+		<SEO title='404 Page' />
+		<PageNotFoundStyles>
+			<div className='container'>
+				<h1>Oops… Deze pagina is niet beschikbaar!</h1>
+			</div>
+		</PageNotFoundStyles>
+	</>
+);
+
+export default PageNotFound;
+
+const PageNotFoundStyles = styled.div`
+	margin-top: 20px;
+	display: flex;
+	align-items: center;
+	width: 100vw;
+	height: 90vh;
+	background-image: url(${notFoundImage});
+	background-size: cover;
+	background-position: center top;
+
+	h1 {
+		font-size: 50px;
+		width: 90%;
+	}
+
+	@media ${breakpoint.md} {
+		margin-top: 0px;
+
+		h1 {
+			font-size: 40px;
+		}
+	}
+`;
