8f09491b0f687ba3cea559410ef3ff67e56d46db
HTHmedia <75879530+hthmedia@users.noreply.github.com>
2021-01-12 00:21:26 +0100

    add wwwhthmedia icon

--- gatsby/src/components/layout/Footer.js
+++ gatsby/src/components/layout/Footer.js
@@ -5,90 +5,91 @@ import { breakpoint } from '../../styles/breakpoints';
 import StoreContact from '../store/StoreContact';
 import SocialLinks from '../nav/SocialLinks';
 import StoreHours from '../store/StoreHours';
+import WwwHTHmedia from '../../assets/images/icons/WwwHTH.nl';
 
 export default function Footer({ footerInfo }) {
-	const currentYear = new Date().getFullYear();
-	const [{ winkel }] = footerInfo.nodes;
-	const {
-		contactInfo,
-		storeHours,
-		socialMediaData,
-		storeName,
-		slogan,
-	} = winkel;
+  const currentYear = new Date().getFullYear();
+  const [{ winkel }] = footerInfo.nodes;
+  const {
+    contactInfo,
+    storeHours,
+    socialMediaData,
+    storeName,
+    slogan,
+  } = winkel;
 
-	return (
-		<FooterStyles>
-			<div className='container'>
-				<div className='footer'>
-					<div className='contact'>
-						<StoreContact contact={{ contactInfo, storeName, slogan }} />
-					</div>
-					<div className='social'>
-						<SocialLinks links={socialMediaData} />
-					</div>
-					<div className='store-hours'>
-						<StoreHours storeHours={storeHours} />
-					</div>
-				</div>
-				<div className='copyright'>
-					<p className=''>
-						&copy; Het Arendshuys&nbsp;<span>• {currentYear}</span>
-					</p>
-					<a href='https://www.hth.media'>www.hth.media</a>
-				</div>
-			</div>
-		</FooterStyles>
-	);
+  return (
+    <FooterStyles>
+      <div className='container'>
+        <div className='footer'>
+          <div className='contact'>
+            <StoreContact contact={{ contactInfo, storeName, slogan }} />
+          </div>
+          <div className='social'>
+            <SocialLinks links={socialMediaData} />
+          </div>
+          <div className='store-hours'>
+            <StoreHours storeHours={storeHours} />
+          </div>
+        </div>
+        <div className='copyright'>
+          <p className=''>
+            &copy; Het Arendshuys&nbsp;<span>• {currentYear}</span>
+          </p>
+          <a href='https://www.hth.media'>
+            <WwwHTHmedia
+              color={`var(--light-yellow)`}
+              hoverColor={`var(--yellow)`}
+            />
+          </a>
+        </div>
+      </div>
+    </FooterStyles>
+  );
 }
 
 const FooterStyles = styled.footer`
-	background-color: var(--blue);
-	padding: 3rem 0;
+  background-color: var(--blue);
+  padding: 3rem 0;
 
-	.footer {
-		display: flex;
-		justify-content: space-between;
-		margin-bottom: 2rem;
+  .footer {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 2rem;
 
-		.social {
-			margin: auto;
-			width: 20%;
-		}
-	}
-	.copyright {
-		color: var(--light-yellow);
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		font-size: 1rem;
-		letter-spacing: 1px;
+    .social {
+      margin: auto;
+      width: 20%;
+    }
+  }
+  .copyright {
+    color: var(--light-yellow);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    font-size: 1rem;
+    letter-spacing: 1px;
 
-		a {
-			color: var(--light-yellow);
-			text-decoration: none;
-			&:hover,
-			:focus {
-				color: var(--yellow);
-			}
-		}
+    a {
+      margin-top: 0.5rem;
+    }
 
-		img {
-			width: 25px;
-			height: 25px;
-			margin: 0 0.5rem;
-		}
-	}
+    img {
+      width: 25px;
+      height: 25px;
+      margin: 0 0.5rem;
+    }
+  }
 
-	@media ${breakpoint.sm} {
-		.footer {
-			flex-direction: column;
-			align-items: center;
+  @media ${breakpoint.sm} {
+    .footer {
+      flex-direction: column;
+      align-items: center;
 
-			.social {
-				margin: 2rem 0;
-				width: 50%;
-			}
-		}
-	}
+      .social {
+        margin: 2rem 0;
+        width: 50%;
+      }
+    }
+  }
 `;
--- gatsby/src/components/store/StoreContact.js
+++ gatsby/src/components/store/StoreContact.js
@@ -3,55 +3,59 @@ import React from 'react';
 import styled from 'styled-components';
 
 export default function StoreContact({ contact }) {
-	const { address, zipcode, city, telefoonNumber, email } = contact.contactInfo;
-	return (
-		<ContactInfoStyles>
-			<thead>
-				<tr>
-					<th>Adres gegevens</th>
-				</tr>
-			</thead>
-			<tbody>
-				<tr>
-					<td>{`${contact.storeName} - ${contact.slogan}`}</td>
-				</tr>
-				<tr>
-					<td>{address}</td>
-				</tr>
-				<tr>
-					<td>{`${zipcode} ${city}`}</td>
-				</tr>
-				<tr>
-					<td>
-						<a href={`tel:${telefoonNumber}`}>{telefoonNumber}</a>
-					</td>
-				</tr>
-				<tr>
-					<td>
-						<Link to='/contact'>{email}</Link>
-					</td>
-				</tr>
-			</tbody>
-		</ContactInfoStyles>
-	);
+  const { address, zipcode, city, telefoonNumber, email } = contact.contactInfo;
+  return (
+    <ContactInfoStyles>
+      <thead>
+        <tr>
+          <th>Adres gegevens</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{`${contact.storeName} - ${contact.slogan}`}</td>
+        </tr>
+        <tr>
+          <td>{address}</td>
+        </tr>
+        <tr>
+          <td>{`${zipcode} ${city}`}</td>
+        </tr>
+        <tr>
+          <td>
+            <a href={`tel:${telefoonNumber}`}>{telefoonNumber}</a>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Link to='/contact'>{email}</Link>
+          </td>
+        </tr>
+      </tbody>
+    </ContactInfoStyles>
+  );
 }
 
 const ContactInfoStyles = styled.table`
-	color: var(--light-yellow);
-	padding: 0.5rem;
-	font-size: 1.1rem;
+  color: var(--light-yellow);
+  padding: 0.5rem;
+  font-size: 1.1rem;
 
-	th {
-		font-weight: 500;
-		padding-bottom: 0.5rem;
-		text-align: left;
-		color: var(--yellow);
-	}
-	td {
-		padding: 0.2rem 0;
-		font-weight: 300;
-		a {
-			color: var(--light-yellow);
-		}
-	}
+  th {
+    font-weight: 500;
+    padding-bottom: 0.5rem;
+    text-align: left;
+    color: var(--yellow);
+  }
+  td {
+    padding: 0.2rem 0;
+    font-weight: 300;
+
+    a {
+      color: var(--light-yellow);
+      &:hover {
+        color: var(--yellow);
+      }
+    }
+  }
 `;
