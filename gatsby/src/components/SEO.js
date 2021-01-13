9e0e8efd1ecd25b85079519aac476109229a5f58
HTHmedia <75879530+hthmedia@users.noreply.github.com>
2021-01-13 11:48:02 +0100

    add seo brand tags

--- gatsby/src/components/SEO.js
+++ gatsby/src/components/SEO.js
@@ -2,53 +2,59 @@ import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
-export default function SEO({ children, title, image }) {
-	const { site } = useStaticQuery(graphql`
-		query {
-			site {
-				siteMetadata {
-					title
-					description
-					name
-				}
-			}
-		}
-	`);
+export default function SEO({ children, title, image, keywords }) {
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+          description
+          name
+        }
+      }
+    }
+  `);
 
-	return (
-		<Helmet titleTemplate={`%s • ${site.siteMetadata.name}`}>
-			<html lang='nl' />
-			<title>{title || site.siteMetadata.title}</title>
-			{/* Fav Icons */}
-			<link rel='icon' type='image/svg+xml' href='/favicon.svg' />
-			<link rel='alternate icon' href='/favicon.ico' /> {/* fallback */}
-			<link rel='preconnect' href='https://fonts.gstatic.com' />
-			<link
-				href='https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&display=swap'
-				rel='stylesheet'
-			/>
-			{/* Meta Tags */}
-			<meta name='viewport' content='width=device-width, initial-scale=1.0' />
-			<meta charSet='utf-8' />
-			<meta name='description' content={site.siteMetadata.description} />
-			{/* Open Graph */}
-			<meta property='og:image' content={image || '/logo.svg'} />
-			<meta
-				property='og:title'
-				content={title || site.siteMetadata.title}
-				key='ogtitle'
-			/>
-			<meta
-				property='og:site_name'
-				content={site.siteMetadata.title}
-				key='ogsitename'
-			/>
-			<meta
-				property='og:description'
-				content={site.siteMetadata.description}
-				key='ogdescription'
-			/>
-			{children}
-		</Helmet>
-	);
+  return (
+    <Helmet titleTemplate={`%s • ${site.siteMetadata.name}`}>
+      <html lang='nl' />
+      <title>{title || site.siteMetadata.title}</title>
+      {/* Fav Icons */}
+      <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+      <link rel='alternate icon' href='/favicon.ico' /> {/* fallback */}
+      <link rel='preconnect' href='https://fonts.gstatic.com' />
+      <link
+        href='https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&display=swap'
+        rel='stylesheet'
+      />
+      {/* Meta Tags */}
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <meta charSet='utf-8' />
+      <meta name='description' content={site.siteMetadata.description} />
+      <meta
+        name='keywords'
+        content={`${
+          keywords && keywords.join(', ')
+        }, Arendshuys, Cadeauartikelen, Woonaccessoires, Lifestyle, Klassiek met Stijl, Stijlvol Wonen, `}
+      />
+      {/* Open Graph */}
+      <meta property='og:image' content={image || '/logo.svg'} />
+      <meta
+        property='og:title'
+        content={title || site.siteMetadata.title}
+        key='ogtitle'
+      />
+      <meta
+        property='og:site_name'
+        content={site.siteMetadata.title}
+        key='ogsitename'
+      />
+      <meta
+        property='og:description'
+        content={site.siteMetadata.description}
+        key='ogdescription'
+      />
+      {children}
+    </Helmet>
+  );
 }
--- gatsby/src/pages/merken.js
+++ gatsby/src/pages/merken.js
@@ -7,59 +7,61 @@ import { breakpoint } from '../styles/breakpoints';
 import SEO from '../components/SEO';
 
 export const data = graphql`
-	{
-		storeInfo: allSanityWinkel {
-			nodes {
-				id
-				winkel {
-					storeInfo
-					storeImages {
-						asset {
-							id
-							fixed(height: 150, width: 250) {
-								...GatsbySanityImageFixed
-							}
-						}
-					}
-				}
-			}
-		}
-		brands: allSanityBrands(sort: { fields: brandName, order: ASC }) {
-			nodes {
-				id
-				brandName
-				brandInfo
-				slug {
-					current
-				}
-				brandImages {
-					asset {
-						fixed(height: 150, width: 250) {
-							...GatsbySanityImageFixed
-						}
-					}
-				}
-			}
-		}
-	}
+  {
+    storeInfo: allSanityWinkel {
+      nodes {
+        id
+        winkel {
+          storeInfo
+          storeImages {
+            asset {
+              id
+              fixed(height: 150, width: 250) {
+                ...GatsbySanityImageFixed
+              }
+            }
+          }
+        }
+      }
+    }
+    brands: allSanityBrands(sort: { fields: brandName, order: ASC }) {
+      nodes {
+        id
+        brandName
+        brandInfo
+        slug {
+          current
+        }
+        brandImages {
+          asset {
+            fixed(height: 150, width: 250) {
+              ...GatsbySanityImageFixed
+            }
+          }
+        }
+      }
+    }
+  }
 `;
 
 export default function merken({ data }) {
-	return (
-		<>
-			<SEO title='Merken' />
-			<MerkenStyles>
-				<SectionTitle title='Onze Merken' bgColor='var(--light-yellow)' />
-				<BrandsList data={data} />
-			</MerkenStyles>
-		</>
-	);
+  console.log(data.brands.nodes);
+  const brands = data.brands.nodes.map((brand) => brand.brandName);
+  return (
+    <>
+      <SEO title='Merken' keywords={brands} />
+      <MerkenStyles>
+        <SectionTitle title='Onze Merken' bgColor='var(--light-yellow)' />
+        <BrandsList data={data} />
+      </MerkenStyles>
+    </>
+  );
 }
 
 const MerkenStyles = styled.div`
-	margin-top: 100px;
+  margin-top: 100px;
 
-	@media ${breakpoint.sm} {
-		margin-top: 0;
-	}
+  @media ${breakpoint.sm} {
+    margin-top: 0;
+  }
 `;
